d4fc91aad237857cce450bdf9befd177ca4f2f29
Hernan Santarelli <hernansantarelli87@gmail.com>
2023-08-07 20:47:57 -0300

    The entities classes are done

--- src/main/java/logica/Horario.java
+++ src/main/java/logica/Horario.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package logica;
 
-/**
- *
- * @author HERNI
- */
 public class Horario {
+    
+    private int id_Horario;
+    private String horario_inicio;
+    private String horario_fin;
+
+    public Horario() {
+    }
+
+    public Horario(int id_Horario, String horario_inicio, String horario_fin) {
+        this.id_Horario = id_Horario;
+        this.horario_inicio = horario_inicio;
+        this.horario_fin = horario_fin;
+    }
+
+    public int getId_Horario() {
+        return id_Horario;
+    }
+
+    public void setId_Horario(int id_Horario) {
+        this.id_Horario = id_Horario;
+    }
+
+    public String getHorario_inicio() {
+        return horario_inicio;
+    }
+
+    public void setHorario_inicio(String horario_inicio) {
+        this.horario_inicio = horario_inicio;
+    }
+
+    public String getHorario_fin() {
+        return horario_fin;
+    }
+
+    public void setHorario_fin(String horario_fin) {
+        this.horario_fin = horario_fin;
+    }
+    
+    
     
 }
